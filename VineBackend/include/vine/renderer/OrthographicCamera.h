16597f4670745ec462635e4a8c71a0a5b406fa81
Rafay Kashif <kashifrafay075@gmail.com>
2024-08-08 22:50:12 -0400

    Finished orthographic camera

---
+++ VineBackend/include/vine/renderer/OrthographicCamera.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace vine
+{
+    class OrthographicCamera
+    {
+    public:
+        OrthographicCamera(float left, float right, float bottom, float top);
+        ~OrthographicCamera();
+
+        void setProjection(float left, float right, float bottom, float top);
+
+        const glm::vec3& getPosition() const { return position_; }
+        void setPosition(const glm::vec3& position) { position_ = position; updateViewMatrix(); }
+
+        float getRotation() const { return rotation_; }
+        void setRotation(float rotation) { rotation_ = rotation; updateViewMatrix(); }
+
+        const glm::mat4& getProjectionMatrix() const { return projectionMatrix_; }
+        const glm::mat4& getViewMatrix() const { return viewMatrix_; }
+        const glm::mat4& getViewProjectionMatrix() const { return viewProjectionMatrix_; }
+
+    private:
+        void updateViewMatrix();
+
+    private:
+        glm::vec3 position_;
+        float rotation_;
+
+        glm::mat4 projectionMatrix_;
+        glm::mat4 viewMatrix_;
+        glm::mat4 viewProjectionMatrix_;
+    };
+}
---
+++ VineBackend/src/renderer/OrthographicCamera.cpp
@@ -0,0 +1,30 @@
+#include <vine/renderer/OrthographicCamera.h>
+
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace vine
+{
+    OrthographicCamera::OrthographicCamera(float left, float right, float bottom, float top)
+        : position_(glm::vec3(0.0f, 0.0f, 0.0f)), rotation_(0.0f), projectionMatrix_(glm::ortho(left, right, bottom, top))
+    {
+        updateViewMatrix();
+    }
+
+    OrthographicCamera::~OrthographicCamera()
+    {
+    }
+
+    void OrthographicCamera::setProjection(float left, float right, float bottom, float top)
+    {
+        projectionMatrix_ = glm::ortho(left, right, bottom, top);
+        updateViewMatrix();
+    }
+
+    void OrthographicCamera::updateViewMatrix()
+    {
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position_) * glm::rotate(glm::mat4(1.0f), rotation_, glm::vec3(0.0f, 0.0f, 1.0f));
+
+        viewMatrix_ = glm::inverse(transform);
+        viewProjectionMatrix_ = projectionMatrix_ * viewMatrix_;
+    }
+}
