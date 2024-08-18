2e3e2e8924bdb4069d64f0d7894b8b328054e008
Rafay Kashif <kashifrafay075@gmail.com>
2024-08-18 19:59:30 -0400

    began stripping application down (really shouldve done this earlier... thats why vinemain exists)

--- VineBackend/src/core/Application.cpp
+++ VineBackend/src/core/Application.cpp
@@ -24,10 +24,6 @@
 
 namespace vine
 {
-    Renderable* quad;
-
-    Text* text;
-
     Application::Application()
     {
         Logger::init();
@@ -50,28 +46,20 @@ namespace vine
 
         running_ = true;
 
-        SpriteState state;
-        state.pos = { 500.0f, 100.0f };
-        state.scale = { 50.0f, 50.0f };
-        state.layer = 1.0f;
-        state.rotation = 0.0f;
-
-        //sheet = new SpriteSheet("assets/spritesheets/demo/sheet.xml");
-        //Sprite* s = sheet->getSprite("wall_texture_gold.png");
-
         createSpritesFromSheet("assets/spritesheets/demo/sheet.xml");
         Renderable* s = RenderableManager::ref().getRenderable("wall_texture_gold.png");
 
         s->setPosition({ 200.0f, 200.0f });
         s->setScale({ 100.0f, 100.0f });
 
-        quad = new Quad(RenderableState());
+        Quad* quad = new Quad(RenderableState());
         quad->setPosition({ 600.0f, 500.0f });
         quad->setScale({ 100.0f, 100.0f });
         quad->setColor({ 1.0f, 0.0f, 0.0f, 1.0f });
+        RenderableManager::ref().addRenderable("Quad", quad);
 
         Handle handle = ResourceManager::ref().createAndLoadResource<ResourceFont>({ "assets/fonts/opensans/OpenSans-Regular.ttf" });
-        text = new Text(handle, TextState());
+        Text* text = new Text(handle, TextState());
         text->setPosition({ 0.0f, 600.0f });
         text->setLayer(1.0f);
         text->setScale({ 50.0f,50.0f });
@@ -83,7 +71,6 @@ namespace vine
 
     Application::~Application()
     {
-        delete quad;
         RenderableManager::shutdown();
         Renderer::shutdown();
         delete window_;
@@ -111,10 +98,7 @@ namespace vine
         Renderer::ref().beginScene(cam);
         // z is how many units away from the screen --> maybe renderer can handle the flip?
         RenderableManager::ref().render();
-        quad->render();
         Renderer::ref().endScene();
-
-
 
         window_->tick();
     }
